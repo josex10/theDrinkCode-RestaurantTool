a472c4998931c54c1c7112b3218fa0ae3866aeb3
Jose Manuel Badilla Porras <jose.badilla@firstfactory.com>
2023-07-19 22:50:57 -0600

    Initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# theDrinkCode-RestaurantTool
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button variant="contained">Hello World</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
